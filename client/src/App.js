3aae6353eed37ad93824e3c4f73ea57e13816bfe
Dev Baweja <18ucs081@lnmiit.ac.in>
2021-01-16 08:11:28 +0530

    Public

--- client/src/App.js
+++ client/src/App.js
@@ -1,72 +1,68 @@
-import './App.scss';
-
 function App() {
     return (
-        <div className="App">
-            <div class="app">
-                <div class="container">
-                    {/* <!-- Header --> */}
-                    <header class="header"></header>
-                    {/* <!-- /Header --> */}
+        <div className="app">
+            <div className="container">
+                {/* <!-- Header --> */}
+                <header className="header"></header>
+                {/* <!-- /Header --> */}
 
-                    {/* <!-- Navbar --> */}
-                    <nav class="navbar"></nav>
-                    {/* <!-- /Navbar --> */}
+                {/* <!-- Navbar --> */}
+                <nav className="navbar"></nav>
+                {/* <!-- /Navbar --> */}
 
-                    {/* <!-- Main --> */}
-                    <main class="main">
-                        {/* <!-- Chat Panel --> */}
-                        <div class="chat-panel"></div>
-                        {/* <!-- Chat Box --> */}
-                        <div class="chat-box"></div>
-                        {/* <!-- Chat Profile --> */}
-                        <div class="chat-profile"></div>
-                    </main>
-                    {/* <!-- /Main --> */}
+                {/* <!-- Main --> */}
+                <main className="main">
+                    {/* <!-- Chat Panel --> */}
+                    <div className="chat-panel"></div>
+                    {/* <!-- Chat Box --> */}
+                    <div className="chat-box"></div>
+                    {/* <!-- Chat Profile --> */}
+                    <div className="chat-profile"></div>
+                </main>
+                {/* <!-- /Main --> */}
 
-                    {/* <!-- Footer --> */}
-                    <footer class="footer">
-                        <div class="copyright">&copy; Chat Fuel. All rights reserved.</div>
-                        <div class="created-by">
-                            <p>
-                                Created by
-                                <span class="developers">Jayesh</span>
-                                &#38;
-                                <span class="developers">Dev</span>
-                                with
-                            </p>
-                            <svg
-                                class="created-by--svg"
-                                dangerouslySetInnerHTML={{
-                                    __html: '<use xlink:href="svg/sprite.svg#icon-heart"></use>',
-                                }}
-                            ></svg>
-                        </div>
-                    </footer>
-                    {/* <!-- /Footer --> */}
-                </div>
-                {/* <!-- /Container --> */}
-
-                {/* <!-- Loading --> */}
-                <div class="loading"></div>
-                {/* <!-- /Loading --> */}
-
-                {/* <!-- Guide --> */}
-                <div class="guide"></div>
-                {/* <!-- /Guide --> */}
-
-                {/* <!-- Alerts --> */}
-                <div class="alerts"></div>
-                {/* <!-- /Alerts --> */}
-
-                {/* <!-- Forms --> */}
-                <div class="forms"></div>
-                {/* <!-- /Forms --> */}
-
-                {/* <!-- Dropdowns --> */}
-                <div class="dropdowns"></div>
-                {/* <!-- /Dropdowns --> */}
+                {/* <!-- Footer --> */}
+                <footer className="footer">
+                    <div className="copyright">&copy; Chat Fuel. All rights reserved.</div>
+                    <div className="created-by">
+                        <p>
+                            Created by
+                            <span className="developers"> Jayesh </span>
+                            &#38;
+                            <span className="developers"> Dev </span>
+                            with
+                        </p>
+                        <svg
+                            className="created-by--svg"
+                            dangerouslySetInnerHTML={{
+                                __html: '<use xlink:href="svg/sprite.svg#icon-heart"></use>',
+                            }}
+                        ></svg>
+                    </div>
+                </footer>
+                {/* <!-- /Footer --> */}
             </div>
+            {/* <!-- /Container --> */}
+
+            {/* <!-- Loading --> */}
+            <div className="loading"></div>
+            {/* <!-- /Loading --> */}
+
+            {/* <!-- Guide --> */}
+            <div className="guide"></div>
+            {/* <!-- /Guide --> */}
+
+            {/* <!-- Alerts --> */}
+            <div className="alerts"></div>
+            {/* <!-- /Alerts --> */}
+
+            {/* <!-- Forms --> */}
+            <div className="forms"></div>
+            {/* <!-- /Forms --> */}
+
+            {/* <!-- Dropdowns --> */}
+            <div className="dropdowns"></div>
+            {/* <!-- /Dropdowns --> */}
         </div>
     );
 }
